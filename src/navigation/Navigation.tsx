ffb1a4b11d23dfcaa4f3d363ba579b57cb5c5cf0
pramodpariharVinivia <umesh.v@sphinxworldbiz.com>
2023-05-30 15:42:53 +0530

    folder rename

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import Home from '../screens/Home/Home';
-import ChooseAccountType from '../screens/Registration/ChooseAccountType';
-import EmailOTPVerify from '../screens/Registration/EmailOTPVerify';
-import EnterEmail from '../screens/Registration/EnterEmail';
-import EnterMobileNumber from '../screens/Registration/EnterMobileNumber';
-import MobileNumberOTPVerify from '../screens/Registration/MobileNumberOTPVerify';
+import Home from '../screens/home/Home';
+import ChooseAccountType from '../screens/registration/ChooseAccountType';
+import EmailOTPVerify from '../screens/registration/EmailOTPVerify';
+import EnterEmail from '../screens/registration/EnterEmail';
+import EnterMobileNumber from '../screens/registration/EnterMobileNumber';
+import MobileNumberOTPVerify from '../screens/registration/MobileNumberOTPVerify';
 
 const Stack = createStackNavigator();
 
