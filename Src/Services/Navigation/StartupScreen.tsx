b9f6ae09f3a38abca2cf3fb8b7f75b52bff711d4
pramodsphinx <pramod.p@sphinxworldbiz.com>
2023-08-10 15:46:59 +0530

    refreshToken within refresh session time

--- Src/Services/Navigation/StartupScreen.tsx
+++ Src/Services/Navigation/StartupScreen.tsx
@@ -1,28 +1,25 @@
 import { StyleSheet, View } from 'react-native';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthStack } from './Navigation';
 import { useLogin } from './LoginProvider';
 import { Splash } from '@Pages/Splash';
 import { NetworkService } from '@Services/Network/NetworkService';
-import { clearLocalStorage, retrieveUserSession } from '@Services/Storage';
 import { Constants } from '@Helpers/Constants';
-import { isEmpty } from '@Helpers/Utility/Utils';
-import LoginScreen from '@Pages/Registration/LoginUser1';
-import TestLogin from '../../Pages/Registration/TestLogin'
 import { VerifyPin } from './VerifyPin';
+import { AgentAPI } from '@Services/API/Agent';
 
 export function StartupScreen() {
   const { isLoggedIn } = useLogin();
   const [appLoading, setAppLoading] = useState(true);
-  const hasPin = React.useRef(false);
+  const storedPassword = React.useRef(false);
   const initialRoute = React.useRef('')
 
   
   useEffect(() => {
+    AgentAPI.Account.RestartActiveSession();
     const initialSetup = async () => {
-      const storedPassword = await retrieveUserSession(Constants.Authentication.PinKey); 
-      initialRoute.current = !isEmpty(storedPassword) ? 'VerifyPin' : 'ChooseAccoutType';  
-      hasPin.current = !isEmpty(storedPassword)
+      storedPassword.current = await retrieveUserSession(Constants.Authentication.PinKey); 
+      initialRoute.current = storedPassword.current ? 'VerifyPin' : 'ChooseAccoutType';  
       setAppLoading(false);
     }
     setTimeout(() => {
@@ -30,9 +27,9 @@ export function StartupScreen() {
     }, 100);
   }, [appLoading]);
 
-  useEffect(() => {}, [hasPin])
+  useEffect(() => {}, [storedPassword.current])
 
-  if (!appLoading && hasPin.current) {
+  if (!appLoading && storedPassword.current) {
     return <VerifyPin />
   }
   if (appLoading) {
